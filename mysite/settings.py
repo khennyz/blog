e507755c309dd6bc4254c9b419c10fdd91c7eaa8
khennyz <skascie@gmail.com>
2017-07-27 16:18:48 +0000

    backup su git e poi locale.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u-vqe@(!+m9-x*$1$wcmf(#j=_kqa%hp-k0=ki$@f=0iw$x_^l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['khennyz.pythonanywhere.com']
 
 
 # Application definition
